c9907637405955785ad83142aa031db4ee83675a
z004d4fc <zpgru3tst6l6qowsfbq5ruxb4x24dch5zgb6ncz6xz6rdzlkp2fa>
2023-08-22 15:34:02 +0800

    cmake 相关

---
+++ zyz_net_muduo/cmake/TutorialConfig.h
@@ -0,0 +1,8 @@
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
+
+#define USE_MYMATH
+
+//dose the platform provide exp and log function
+/* #undef HAVE_LOG */
+/* #undef HAVE_EXP */
